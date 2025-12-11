308eb4963e875126cedceff30968aabe628e165a
CreativeMonk <sudeepsureshhuf@gmail.com>
2025-12-11 16:18:35 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import VakClientApp from './VakClientApp.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <VakClientApp />
+  </React.StrictMode>,
+)
 
